e3d51c3207696891e71d69b1702f8011d0f40b3d
burundukoff <burundukoff@net.net>
2020-09-23 16:07:04 +0400

    star

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm 123help at https://www.jetbrains.com/help/pycharm/
+# See PyCharm 123help*********************** at https://www.jetbrains.com/help/pycharm/
